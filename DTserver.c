349398b94319361f91bb33c791536a48ff9c09e7
randallpod <muzakkirifwat@gmail.com>
2022-05-29 00:36:26 +0800

    Individual Assignment

--- DTserver.c
+++ DTserver.c
@@ -13,10 +13,9 @@ int main(int argc, char *argv[] )
   int socTime,new_socket,c, n_client = 0;
   struct sockaddr_in server, client;
 
-   char buffer[1024] = { 0 };
-   char svReply[sizeof(buffer)];
 
-  //creating socket
+
+  //create socket
     socTime = socket(AF_INET,SOCK_STREAM,0);
     if(socTime==-1)
       {
@@ -25,26 +24,26 @@ int main(int argc, char *argv[] )
 
       }
 
-  //preparing for socket address
+  //create socket address
     server.sin_family=AF_INET;
     server.sin_addr.s_addr=INADDR_ANY;
     server.sin_port=htons(13); //use port 13
 
-  //bind
+  //binding
     if(bind(socTime,(struct sockaddr *)&server, sizeof(server))<0)
       {
 
        puts("\nbind failed");
        return 1;
       }
-     puts("\nSocket is successfully binded! ");
+     puts("\nSocket is  binded! ");
 
 
   //listen
     listen (socTime,3);
 
-  //accept for incoming connection
-    puts("\nWaiting for incoming connections....");
+  //to accept incoming connection
+    puts("\nPlease wait for  connections....");
     c=sizeof(struct sockaddr_in);
 
 
@@ -52,25 +51,21 @@ int main(int argc, char *argv[] )
     new_socket=accept(socTime,(struct sockaddr *)&client,(socklen_t*)&c);
     if(new_socket<0)
       {
-       perror("\nFAILED TO ACCEPT");
+       perror("\nCONNECTION FAILED!");
        return 1;
       }
-    puts("Connection accepted!");
-
- // while(1){
-    //Counting the request from client
-   // n_client++;
+    puts("CONNECTION ACCEPTED!");
 
     //Getting current time
     time_t currentTime;
     time(&currentTime);
 
-    //Sending time to client
+    //Send time to client
     printf("Sending Client current time = %s ", ctime(&currentTime));
     send(new_socket, ctime(&currentTime), 30, 0);
  // }
 
-  //Closing socket
+  //Close socket
     close(new_socket);
 
 return 0;
